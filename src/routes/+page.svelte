30112401668997b51b0b8ca43fe9ff4805d9cd41
Andrew Lau <andrewlau2019@gmail.com>
2023-04-25 01:56:10 -0700

    setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+<script>
+    import '../app.css';
+	import Champion from '../components/champion.svelte';
+    console.log("Champion List Page");
+</script>
+
+<h1>Champion List</h1>
+<Champion />
+
+<style>
+
+</style>
