1a0ec3c138dc958cb519c19c1d2fdccc133472c4
Stanley Chong <73448184+stanley54073@users.noreply.github.com>
2023-05-10 01:53:28 -0700

    minor additions to champion page information

--- src/routes/[championId]/+page.svelte
+++ src/routes/[championId]/+page.svelte
@@ -5,16 +5,28 @@
     const championName = Object.keys(champion.data);
 </script>
 
-<h1>{champion.data[championName].name}</h1>
-<p style = "text-transform:capitalize;">{champion.data[championName].title}</p>
-<p>{champion.data[championName].lore}</p>
+<h1>{champion.data[championName].name}</h1> 
+
+<p style = "text-transform:capitalize;"> <b>Title:</b> {champion.data[championName].title}</p>
+<p> <b>Lore:</b> {champion.data[championName].lore}</p>
 <img src={`http://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/${champion.data[championName].image.full}`} alt="">
 
-<p>{champion.data[championName].passive.description}</p>
-<p>{champion.data[championName].passive.name}</p>
-
-<p>{champion.data[championName].spells[0].name}</p>
+<p> <b>Passive:</b> {champion.data[championName].passive.name} </p>
+<p> {champion.data[championName].passive.description}</p>
+    
+<p> <b>Q:</b> {champion.data[championName].spells[0].name}</p>
 <p>{champion.data[championName].spells[0].description}</p>
+
+<p> <b>W:</b> {champion.data[championName].spells[1].name}</p>
+<p>{champion.data[championName].spells[1].description}</p>
+
+<p> <b>E:</b> {champion.data[championName].spells[2].name}</p>
+<p>{champion.data[championName].spells[2].description}</p>
+
+<p> <b>R:</b> {champion.data[championName].spells[3].name}</p>
+<p>{champion.data[championName].spells[3].description}</p>
+
 
 
 <button><a href="/">Back to Home</a></button>
+
