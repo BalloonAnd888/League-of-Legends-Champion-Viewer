519a283d9fda9be7fbdb666df5c6caa45f6da99d
Andrew Lau <andrewlau2019@gmail.com>
2023-05-10 21:08:41 -0700

    Champ intro, passive and spells

--- src/routes/[championId]/+page.svelte
+++ src/routes/[championId]/+page.svelte
@@ -3,30 +3,124 @@
     const { champion } = data;
     console.log(champion);
     const championName = Object.keys(champion.data);
+
+    const ability = ["Q", "W", "E", "R"];
+    // console.log(ability)
 </script>
 
-<h1>{champion.data[championName].name}</h1> 
-
-<p style = "text-transform:capitalize;"> <b>Title:</b> {champion.data[championName].title}</p>
-<p> <b>Lore:</b> {champion.data[championName].lore}</p>
-<img src={`http://ddragon.leagueoflegends.com/cdn/13.9.1/img/champion/${champion.data[championName].image.full}`} alt="">
-
-<p> <b>Passive:</b> {champion.data[championName].passive.name} </p>
-<p> {champion.data[championName].passive.description}</p>
+<div class="intro">
+    <div class="introContainer">
+        <h1 class="champName">{champion.data[championName].name}</h1> 
     
-<p> <b>Q:</b> {champion.data[championName].spells[0].name}</p>
-<p>{champion.data[championName].spells[0].description}</p>
+        <p class="champTitle">{champion.data[championName].title}</p>
+    </div>
 
-<p> <b>W:</b> {champion.data[championName].spells[1].name}</p>
-<p>{champion.data[championName].spells[1].description}</p>
+    <div class="champSplashContainer">
+        <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championName}_0.jpg`} class="champSplash" alt="pic">
+    </div>
 
-<p> <b>E:</b> {champion.data[championName].spells[2].name}</p>
-<p>{champion.data[championName].spells[2].description}</p>
+    <div class="champLoreContainer">
+        <p>{champion.data[championName].lore}</p>
+    </div>
+</div>
 
-<p> <b>R:</b> {champion.data[championName].spells[3].name}</p>
-<p>{champion.data[championName].spells[3].description}</p>
+<div class="abilities">
+    <div class="abilityTitle">
+        <h2>Abilities</h2>
+    </div>
+    <div class="passiveContainer">
+        <div class="passiveTitle">
+            <h3>Passive</h3>
+        </div>
+        <div class="passive">
+            <h4>{champion.data[championName].passive.name} </h4>
+            <p> {champion.data[championName].passive.description}</p>
+        </div>
+    </div>
+    <!-- Champion Passive Ability Image (not working for all champion)-->
+    <!-- <img src={`http://ddragon.leagueoflegends.com/cdn/13.9.1/img/passive/${champion.data[championName].passive.image.full}`} alt="passive"> -->
+</div>
+
+<div class="spells">
+    {#each champion.data[championName].spells as spell, index}
+        <div class="spellContainer">
+            <h4 class="spellName">{ability[index]}: {spell.name}</h4>
+            <p class="spellDescription">{spell.description}</p>
+            <img src={`http://ddragon.leagueoflegends.com/cdn/13.9.1/img/spell/${spell.image.full}`} alt="">
+        </div>
+    {/each}
+</div>
 
 
+<!-- {#each champion.data[championName].skins as skin, index}
+    <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${championName}_${index}.jpg`} alt="">
+    <h4>{skin.name}</h4>
+{/each} -->
 
 <button><a href="/">Back to Home</a></button>
+
+<style>
+    .champName {
+        text-align: center;
+    }
+
+    .champTitle {
+        text-transform: capitalize;
+        text-align: center;
+    }
+
+    .introContainer {
+        padding: 20px 0px;
+    }
+
+    .champSplash {
+        max-width: 250px;
+    }
+
+    .champSplashContainer {
+        text-align: center;
+        padding-bottom: 10px;
+    }
+
+    .champLoreContainer {
+        padding: 0px 20px 10px 20px;
+    }
+
+    /* .abilities {
+        
+    } */
+    
+    .abilityTitle {
+        text-align: center;
+        text-decoration: underline;
+        padding-bottom: 10px;
+    }
+
+    .passiveTitle {
+        text-align: center;
+    }
+
+    .passive {
+        text-align: center;
+        padding: 0px 20px;
+    }
+
+    .spells {
+        padding: 20px 10px;
+    }
+
+    .spellContainer {
+        text-align: center;
+        padding: 10px 15px;
+    }
+    
+    .spellName {
+        padding-bottom: 5px;
+    }
+
+    .spellDescription {
+        padding-bottom: 5px;
+    }
+
+</style>
 
